50ee17c472394bc1c6f05ffe1c0a6f2744a2d65f
Ashutosh Tiwari <30868791+Ashutosh7557@users.noreply.github.com>
2023-07-10 22:26:43 +0530

    CoreWebAPI\1\CoreAPICRUD\RestAPICRUDEFwithDatabase
    .Net Core Web API\1\CoreAPICRUD\RestAPICRUDEFwithDatabase

---
+++ RestAPICRUDEFDemo/RestAPICRUDEFDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPICRUDEFDemo.EmployeeData;
+using RestAPICRUDEFDemo.Models;
+
+namespace RestAPICRUDEFDemo.Controllers
+{
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private IEmployeeData _employeeData;
+
+        public EmployeesController(IEmployeeData employeeData)
+        {
+            _employeeData = employeeData;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(_employeeData.GetEmployees());
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetEmployee(Guid id)
+        {
+            var _employee = _employeeData.GetEmployee(id);
+
+            if (_employee != null)
+            {
+                return Ok(_employee);
+            }
+
+            return NotFound($"Employee with Id:{id} was not found.");
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            _employeeData.AddEmployee(employee);
+
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + employee.Id, employee);
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var _employee = _employeeData.GetEmployee(id);
+
+            if(_employee != null)
+            {
+                _employeeData.DeleteEmployee(_employee);
+                return Ok();
+            }
+
+            return NotFound($"Employee with Id:{id} was not found.");
+        }
+
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditEmployee(Guid id, Employee employee)
+        {
+            var _existingemployee = _employeeData.GetEmployee(id);
+
+            if (_existingemployee != null)
+            {
+                employee.Id = _existingemployee.Id;
+                _employeeData.EditEmployee(employee);
+            }
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ RestAPICRUDEFDemo/RestAPICRUDEFDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestAPICRUDEFDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="Name cannot be more than 50 chars")]
+        public string Name { get; set; }
+    }
+}
